a9c2331b6496079b196aea999df416948acfd26e
jeudy <jeudy@redhat.com>
2019-07-18 15:01:10 -0400

    replaced hydrajs getProducts with strata

--- app/cases/services/productsService.js
+++ app/cases/services/productsService.js
@@ -51,11 +51,8 @@ export default class ProductsService {
                 contact = CaseService.virtualOwner; // Used for fetching products list(of customers) for cases managed by Partners
             }
             this.productsLoading = true;
-            return hydrajs.products.getProducts(contact).then(angular.bind(this, function (response) {
-                this.products = response.map((product) => {
-                    product.code = product.name;
-                    return product;
-                });
+            return strataService.products.list(contact).then(angular.bind(this, function (response) {
+                this.products = response;
 
                 this.productsRecentlyFiledAgainst = this.products.filter((product) => product.recentlyFiledAgainst);
                 this.buildProductOptions();
@@ -156,8 +153,8 @@ export default class ProductsService {
                 this.fetchProductDetail(product);
             }
 
-            return hydrajs.products.getProductVersions(product).then(angular.bind(this, function (response) {
-                const versions = response.items;
+            return strataService.products.versions(product).then(angular.bind(this, function (response) {
+                const versions = response;
                 this.versions = versionSort(versions);
                 this.versionDisabled = false;
                 this.versionLoading = false;
