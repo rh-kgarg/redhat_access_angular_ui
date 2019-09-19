afc12160cf8842813b3a68d4f72475728c350046
Jordan Eudy <jeudy100@gmail.com>
2019-09-19 08:25:59 -0400

    fixed untranslated words

--- app/cases/controllers/edit.js
+++ app/cases/controllers/edit.js
@@ -23,7 +23,7 @@ export default class Edit {
         $scope.isShowRmeEscalationBox = false;
         $scope.isCreateRmeEscalationBox = true;
         $scope.ownerTooltip = '';
-        $scope.cepMessage = gettextCatalog.getString("Used by consultants to indicate that a consulting engagement is in progress and the issue requires increased attention from support resources.");
+        $scope.cepMessage = gettextCatalog.getString("Used by consultants to indicate that a consulting engagement is in progress and the issue requires increased attention from support resources");
         $scope.showCasePage = () => securityService.loginStatus.isLoggedIn && !HeaderService.pageLoadFailure && CaseService.sfdcIsHealthy && securityService.loginStatus.userAllowedToManageCases && !$scope.loading.kase;
 
         $scope.init = function () {
